4c0ae2647c67181f55859b811996a4642b8253c8
K E Sivakavin <kesivakavin@gmail.com>
2024-08-10 18:46:10 +0530

    Update overally

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.soiltester import logger
+from src.soiltester.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("this is the soiltester custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
