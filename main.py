b4f61fbe537f3657aa1af594e597e17313ac5190
K E Sivakavin <kesivakavin@gmail.com>
2024-08-15 14:33:00 +0530

    Sample Model Training

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.soiltester import logger
 from src.soiltester.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.soiltester.pipeline.stage_02_prepare_base_model import PrepareBaseTrainingPipeline
+from src.soiltester.pipeline.stage_03_training import ModelTrainingPipeline
+#from src.soiltester.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion"
 try:
@@ -22,3 +24,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Training"
+try: 
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
